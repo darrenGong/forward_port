fbb3a56c61ddabd98f0bb0eee15e3182f4269d78
darren.gong <darren.gong@ucloud.cn>
2016-11-17 11:46:52 +0800

    modify config

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"flag"
 	"forward_port/config"
-	"log"
-	"uframework/log"
-	"forward_port/src"
-	"net"
 	"forward_port/rule"
+	"forward_port/src"
+	"log"
+	"net"
+	"uframework/log"
 )
 
 const (
@@ -15,8 +15,8 @@ const (
 )
 
 var (
-	configFile = flag.String("c", "F:\\go-dev\\src\\forward_port\\config\\config.json", "configuration, json format")
-	ruleFile = "F:\\go-dev\\src\\forward_port\\etc\\Rule_Egi5Th.json"
+	configFile = flag.String("c", "config/config.json", "configuration, json format")
+	ruleFile   = "etc/Rule_Egi5Th.json"
 )
 
 func GetAddrByInterfaceName(interfaceName string) string {
@@ -37,13 +37,11 @@ func main() {
 	flag.Parse()
 
 	Config := config.Config{}
-	if err := config.ParseConfig(*configFile, &Config);
-		err != nil {
+	if err := config.ParseConfig(*configFile, &Config); err != nil {
 		log.Fatalf("Failed to parse config: %s, err: %v\n", *configFile, err)
 	}
 	uflog.InitLogger(Config.LogPath, Config.LogPrefix, "", LOG_SIZE, "DEBUG")
-	//addr := GetAddrByInterfaceName(Config.InterfaceName)
-	addr := "127.0.0.1"
+	addr := GetAddrByInterfaceName(Config.InterfaceName)
 
 	pRule := new(rule.Rule)
 	if err := rule.LoadRule(ruleFile, pRule); err != nil {
